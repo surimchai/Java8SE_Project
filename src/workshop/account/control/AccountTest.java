a42dc33e2118f4feeef095f30fc3d14198f710f8
surimchai <slchai0903@gmail.com>
2026-08-25 15:34:44 +0900

    Account 기본 생성자

---
+++ src/workshop/account/control/AccountTest.java
@@ -0,0 +1,20 @@
+package workshop.account.control;
+
+import workshop.account.entity.Account;
+//ctrl + f11 java run
+public class AccountTest {
+	public static void main(String[] args) {
+		Account account = new Account();
+		
+		//setter 메서드를 호출하여 Account 변수의 값을 변경 요청 
+		//고객번호 : "A1100", 계좌번호 : "221-22-3477", 잔액 : 100000
+		account.setCustId("A1100");
+		account.setAcctId("221-22-3477");
+		account.deposit(100000);
+		
+		System.out.println("고객번호 = " + account.getCustId());
+		System.out.println("계좌번호 = " + account.getAcctId());
+		System.out.println("잔액 = " + account.getBalance());
+		
+	}
+}
--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,5 +1,47 @@
 package workshop.account.entity;
 
 public class Account {
+	private String custId;
+	private String acctId;
+	private int balance;
 
+	//default constructor 선언
+	public Account() {
+		System.out.println("Account 기본 생성자가 호출됨!")
+	}
+	
+	//getter method
+	public String getCustId() {
+		return custId;
+	}
+	//setter
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+
+	public String getAcctId() {
+		return acctId;
+	}
+
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+	
+	//입금
+	public void deposit(int amount) {
+		//this.balance = this.balance + amount;
+		this.balance += amount;
+	}
+	
+	//출금
+	public void withdraw(int amount) {
+		this.balance -= amount;
+	}
+	
+	
+	
 }
